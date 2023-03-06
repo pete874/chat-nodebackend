a07e9cbd436c3ac9a909079b8f60763c92e3c194
Petri Nurmi <98823924+pete874@users.noreply.github.com>
2023-03-07 00:45:36 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const express = require('express');
-const socketio = require('socket.io')({cors: {origin: "*"}});
 const cors = require('cors');
+const socketio = require('socket.io');
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
